36f784e37eacfbcb90563ac6ca9222e797d05bf4
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-10-27 20:46:34 -0300

    autofilling address

--- main.js
+++ main.js
@@ -16,7 +16,14 @@ $(document).ready(function() {
     const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
 
     $.ajax(endpoint).done(function(data) {
-      console.log(data);
+      $.ajax(endpoint).done(function(data) {
+        const logradouro = data.logradouro;
+        const bairro = data.bairro;
+        const cidade = data.localidade;
+        const estado = data.uf;
+        const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        $('#endereco').val(endereco);
+      })
     })
   })
 })
